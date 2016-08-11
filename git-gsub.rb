1f755df68ce40a6f844d0a9801acf5c645fb13be
Guilherme Campos <guilhermekbsa@gmail.com>
2016-08-11 20:35:09 +1000

    Update git-gsub.rb

--- git-gsub.rb
+++ git-gsub.rb
@@ -5,8 +5,6 @@ class GitGsub < Formula
   version "0.1"
   sha256 "54aefe340f46a2b188858ef90f9d144a79fd180308f1969ffa878f982949a5ca"
 
-  depends_on :git
-
   def install
     bin.install "git-gsub"
   end
